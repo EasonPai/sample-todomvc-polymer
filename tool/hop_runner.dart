53fee1e18dd3eb6e5567bf650fdb45be94ed156f
Eason Pai <mediakid@gmail.com>
2015-08-12 10:22:44 +0800

    - renew codeship script and test hop runner

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -12,7 +12,7 @@ main(List<String> args) {
 Task createUnitTestTask() {
 	return new Task((TaskContext tcontext) {
 		tcontext.info("Running Unit Tests....");
-		var result = Process.run('../drt-lucid64-full-stable-45692.0/content_shell',
+		var result = Process.run('./content_shell',
 		['--dump-render-tree','test/mainpage_test.html'])
 		.then((ProcessResult process) {
 			tcontext.info(process.stdout);
